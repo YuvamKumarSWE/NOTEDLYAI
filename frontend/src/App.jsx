f25101b20fc209e9798415b8dec6b07a99ddbb32
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-04-30 00:36:02 -0400

    Removing navbar from all pages except the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-[#1a0223d5]">
-        <Navbar />
-       
         <Routes>
           <Route path="/dashboard" element={<Home />} />
           <Route path="/login" element={<Login />} />
